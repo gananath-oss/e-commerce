e8ac3d7fcf55ad3e9d091d314fb374c9b08a0d2a
gananath-oss <ganaya2k17@gmail.com>
2024-03-19 02:19:55 +0500

    Firebase initialise
    Firebase initialise: For DB

---
+++ src/Firebase/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { getApp, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD8PDPOaQGLk6wykseXp8WnbmXh9wy5TJw",
+  authDomain: "e-commerce-da7ea.firebaseapp.com",
+  projectId: "e-commerce-da7ea",
+  storageBucket: "e-commerce-da7ea.appspot.com",
+  messagingSenderId: "672979094674",
+  appId: "1:672979094674:web:466ec76cf06ca53c29ea4b",
+};
+
+// Initialize Firebase
+const app = getApp.length ? getApp() : initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
