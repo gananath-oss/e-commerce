a7800ffc2e50379c829b269a0de70418215a0c08
gananath-oss <ganaya2k17@gmail.com>
2024-03-21 00:51:30 +0500

    create login/register user page
    -not apply styles yet

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,104 +1,88 @@
+import { useState } from "react";
+import { MainContainer } from "../../Layout/MainContainer";
 import { Button } from "@mui/material";
-import { useEffect, useRef } from "react";
-import { useDispatch } from "react-redux";
-import { collection, query, where, getDocs } from "firebase/firestore/lite";
-import db, { auth } from "../../Firebase/firebase";
-import { addUser } from "../../Store/ReduxSlice/userSlice";
-import { useNavigate } from "react-router-dom";
-import {
-  onAuthStateChanged,
-  signInWithEmailAndPassword,
-  signOut,
-} from "firebase/auth";
 
 const Login = () => {
-  const dispatch = useDispatch();
-
-  const usernameRef = useRef();
-  const passwordRef = useRef();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const userCheck = () => {
-      onAuthStateChanged(auth, (user) => {
-        if (user) {
-          // User is signed in, see docs for a list of available properties
-          // https://firebase.google.com/docs/reference/js/auth.user
-          const uid = user.uid;
-          console.log(user);
-          // ...
-        } else {
-          console.log("User checked run : ", user);
-          // User is signed out
-          // ...
-        }
-      });
-    };
-
-    userCheck();
-
-    return () => userCheck();
-  }, []);
-
-  const loginSubmit = () => {
-    const email = usernameRef.current.value;
-    const password = passwordRef.current.value;
-
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        // console.log(user);
-      })
-      .catch((error) => {
-        // const errorCode = error.code;
-        const errorMessage = error.message;
-        // console.log(errorMessage);
-      });
-
-    // const q = query(collection(db, "users"), where("usernameRef", "==", "admin"));
-
-    // getDocs(q)
-    //   .then((querySnapshot) => {
-    //     querySnapshot.forEach((doc) => {
-    //       console.log(doc.id, " => ", doc.data());
-    //       const userData = doc.data();
-    //       dispatch(addUser(userData));
-    //       navigate("/admin");
-    //     });
-    //   })
-    //   .catch((error) => {
-    //     console.log("Error getting documents: ", error);
-    //   });
-  };
-
-  const logout = () => {};
-
+  const [select, setSelect] = useState("login");
   return (
-    <div>
-      <h1>Loging</h1>
-      <br />
-      <input ref={usernameRef} type="email" placeholder="e-mail" />
-      <input ref={passwordRef} type="password" placeholder="Password" />
-      <Button onClick={loginSubmit}>Login</Button>
-      <br />
-      <Button
-        onClick={() => {
-          signOut(auth)
-            .then(() => {
-              // Sign-out successful.
-              alert("User sign out");
-            })
-            .catch((error) => {
-              // An error happened.
-              alert(error.message);
-            });
-        }}
-      >
-        Logout
-      </Button>
-    </div>
+    <MainContainer>
+      {select === "login" ? (
+        <LoginComponent />
+      ) : select === "register" ? (
+        <RegisterComponent />
+      ) : null}
+      {/* change login or selector */}
+      <div className=" mt-5">
+        {select === "login" ? (
+          <p>
+            {" "}
+            Don't have an account?{" "}
+            <span
+              className=" cursor-pointer"
+              onClick={() => setSelect("register")}
+            >
+              Sign Up
+            </span>
+          </p>
+        ) : select === "register" ? (
+          <p>
+            Already have an account{" "}
+            <span
+              className=" cursor-pointer"
+              onClick={() => setSelect("login")}
+            >
+              Sign In
+            </span>
+          </p>
+        ) : null}
+      </div>
+    </MainContainer>
   );
 };
 
 export default Login;
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+const LoginComponent = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+    </div>
+  );
+};
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+const RegisterComponent = () => {
+  const userRegisterHandle = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+  };
+  return (
+    <div>
+      <h1>User Register</h1>
+      <form onSubmit={userRegisterHandle}>
+        <div>
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+        </div>
+        <input type="email" placeholder="Email Address" />
+        <input type="password" placeholder="Password" />
+        <input type="password" placeholder="Confirmed Password" />
+        <input type="text" placeholder="Phone Number" />
+        <Button type="submit">Create</Button>
+      </form>
+    </div>
+  );
+};
